5bffafb95a3a42d9337b765200d465566c2e6979
Dimitri <dimitri.guitton.dev@gmail.com>
2025-03-19 10:56:54 +0100

    Add POST, PUT and DELETE mapping

--- src/main/java/com/example/sbbeginnerapi/controller/EmployeeController.java
+++ src/main/java/com/example/sbbeginnerapi/controller/EmployeeController.java
@@ -3,13 +3,41 @@ package com.example.sbbeginnerapi.controller;
 import com.example.sbbeginnerapi.model.Employee;
 import com.example.sbbeginnerapi.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
+
     @Autowired
     private EmployeeService employeeService;
+
+    /**
+     * Create - Add a new employee
+     * @param employee An object employee
+     * @return The employee object saved
+     */
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+
+    /**
+     * Read - Get one employee
+     * @param id The id of the employee
+     * @return An Employee object full filled
+     */
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") final Long id) {
+        Optional<Employee> employee = employeeService.getEmployee(id);
+        if(employee.isPresent()) {
+            return employee.get();
+        } else {
+            return null;
+        }
+    }
 
     /**
      * Read - Get all employees
@@ -18,5 +46,49 @@ public class EmployeeController {
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
+    }
+
+    /**
+     * Update - Update an existing employee
+     * @param id - The id of the employee to update
+     * @param employee - The employee object updated
+     */
+    @PutMapping("/employee/{id}")
+    public Employee updateEmployee(@PathVariable("id") final Long id, @RequestBody Employee employee) {
+        Optional<Employee> e = employeeService.getEmployee(id);
+        if(e.isPresent()) {
+            Employee currentEmployee = e.get();
+
+            String firstName = employee.getFirstName();
+            if(firstName != null) {
+                currentEmployee.setFirstName(firstName);
+            }
+            String lastName = employee.getLastName();
+            if(lastName != null) {
+                currentEmployee.setLastName(lastName);;
+            }
+            String mail = employee.getMail();
+            if(mail != null) {
+                currentEmployee.setMail(mail);
+            }
+            String password = employee.getPassword();
+            if(password != null) {
+                currentEmployee.setPassword(password);;
+            }
+            employeeService.saveEmployee(currentEmployee);
+            return currentEmployee;
+        } else {
+            return null;
+        }
+    }
+
+
+    /**
+     * Delete - Delete an employee
+     * @param id - The id of the employee to delete
+     */
+    @DeleteMapping("/employee/{id}")
+    public void deleteEmployee(@PathVariable("id") final Long id) {
+        employeeService.deleteEmployee(id);
     }
 }
